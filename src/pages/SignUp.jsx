79d314055972bb745f3f160d009c58154c42c196
JorgeVargas <javargas216@unicauca.edu.co>
2022-05-19 23:10:17 -0500

    Sign Up function

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,34 +1,90 @@
-import React from "react";
+import React, { useContext, useRef } from "react";
+import { post } from "../api";
+import { authContext } from "../context/AuthContext";
 import { NavLink } from "react-router-dom";
 import Navbar from "../components/Navbar";
 
 export default function SignUp() {
+	const { setAuth } = useContext(authContext);
+
+	const nameRef = useRef();
+	const emailRef = useRef();
+	const passwordRef = useRef();
+	const roleRef = useRef();
+
+	const signup = (e) => {
+		e.preventDefault();
+		post("/api/auth/signup", {
+			name: nameRef.current.value,
+			email: emailRef.current.value,
+			password: passwordRef.current.value,
+			role: roleRef.current.value,
+		})
+		.then(({ data }) => {
+			data.error ? 
+			console.log(data.error) : 
+			localStorage.setItem("token", data.token);
+			setAuth({
+				id: data.user.id,
+				name: data.user.name,
+				logged: true,
+			});
+		})
+		.catch((err) => console.log(err));
+	};
+
+	// const recoverSession = () => {
+	// 	const token = localStorage.getItem("token");
+	// 	if (token) {
+	// 		postWithToken("/api/auth/validate", { token })
+	// 		.then(({ data }) => {
+	// 			data.error ? 
+	// 			console.log(data.error) :
+	// 			setAuth({
+	// 				id: data.user.id,
+	// 				name: data.user.name,
+	// 				logged: true,
+	// 			});
+	// 		})
+	// 		.catch((err) => console.log(err));
+	// 	}
+	// };
+
 	return (
 		<>
-			<Navbar/>
-			<div className="container">
+			<Navbar />
+			<form onSubmit={signup} className="container">
 				<h4 className="subtitle">Sign Up</h4>
 
 				<div className="form-group">
 					<label className="lbl">Name</label>
-					<input className="inp" placeholder="Jhon Doe" type="text" />
+					<input ref={nameRef} className="inp" placeholder="Jhon Doe" type="text" />
 				</div>
 
 				<div className="form-group">
 					<label className="lbl">Email address</label>
-					<input className="inp" placeholder="jhondoe@domain.com" type="email" />
+					<input
+						ref={emailRef}
+						className="inp"
+						placeholder="jhondoe@domain.com"
+						type="email"
+					/>
 				</div>
 
 				<div className="form-group">
 					<label className="lbl">Password</label>
-					<input className="inp" type="password" />
+					<input ref={passwordRef} className="inp" type="password" />
 				</div>
 
 				<div className="form-group">
 					<label className="lbl">Role</label>
-					<select className="inp">
-						<option className="opt" value="applicant">Applicant</option>
-						<option className="opt" value="employer">Employer</option>
+					<select ref={roleRef} className="inp">
+						<option className="opt" value="applicant">
+							Applicant
+						</option>
+						<option className="opt" value="employer">
+							Employer
+						</option>
 					</select>
 				</div>
 
@@ -44,7 +100,10 @@ export default function SignUp() {
 						Login here!
 					</NavLink>
 				</div>
-			</div>
+			</form>
+			{/* <button onClick={recoverSession} className="btn">
+				<span>Recover Ression</span>
+			</button> */}
 		</>
 	);
 }
