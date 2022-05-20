d4e727c890f07a674a48db428253c5cfcad22fad
LauraMamian <lauramamianc@unicauca.edu.co>
2022-05-20 01:06:09 -0500

    search job

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,13 @@ import Navbar from "../components/Navbar";
 export default function Home() {
 	return (
 		<>
-			<Navbar/>
+			<Navbar />
 			<div className="container">
-				<h2>Find your dream jobs with <h1>J&L</h1></h2>
+				<div className="trabajo">
+					<input type="text" placeholder="Job, Company name..." />
+					<button type="button">Find!</button>
+				</div>
+
 				{/* <p className="tag">
 					Javascript
 				</p> */}
