26b54bb6b8378075befca2f0473cccd3568642ad
JorgeVargas <javargas216@unicauca.edu.co>
2022-05-19 22:25:34 -0500

    Authentication Context

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { useContext, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {postWithToken} from './api';
+import { authContext } from './context/AuthContext';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 
 function App() {
+
+	const context = useContext(authContext);
+
+	useEffect(() => {
+		postWithToken('/api/auth/validate')
+		.then(({data})=>{
+			data.failed?
+			console.log(data) :
+			context.setAuth({
+				id:data.user.id,
+				name:data.user.name,
+				logged:true
+			})
+		})	
+	}, [])
+
 	return (
 		<div className="App">
 			<Router>
