3137001410a13bb0f8cb12ff2248b1f7a4b77c70
JorgeVargas <javargas216@unicauca.edu.co>
2022-05-19 22:15:52 -0500

    Axios, post and post with token

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import { Spin as Hamburger } from 'hamburger-react'
-import { NavLink } from "react-router-dom";
 import NavLinks from './NavLinks';
 
 export default function Navbar() {
