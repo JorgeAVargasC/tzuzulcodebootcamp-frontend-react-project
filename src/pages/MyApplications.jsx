42f63ba06df8ce699f1c79ee8cc65a324603147e
JorgeVargas <javargas216@unicauca.edu.co>
2022-05-21 19:23:09 -0500

    My applications list

--- src/pages/MyApplications.jsx
+++ src/pages/MyApplications.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { postWithToken } from "../api";
+import Navbar from "../components/Navbar";
+import Jobs from "./Jobs";
+
 
 export default function MyApplications() {
-  return (
-    <div>
-        <h1>MY JOBS</h1>
-    </div>
-  )
+
+    const [applications, setApplications] = useState([])
+
+    useEffect(() => {
+        postWithToken("/api/jobs/me", {
+            token: localStorage.getItem("token")
+        })
+        .then(({ data }) => {
+            console.log(data);
+            setApplications(data);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }, []);
+
+	return (
+        
+		<>
+            <Navbar/>
+            <div className="container">
+                <h4>My Applications</h4>
+                {applications.map((application) => {
+                    return (
+                        <div className="job-container">
+                            <div className="job-title">
+                                <h3>{application.title}</h3>
+                                <p>{application.salary}</p>
+                            </div>
+                            {/* <button onClick={() => viewJob(application._id)} className="btn">
+								<span>Ver</span>
+							</button> */}
+                        </div>
+                    )
+                })}
+            </div>
+			
+		</>
+	);
 }
