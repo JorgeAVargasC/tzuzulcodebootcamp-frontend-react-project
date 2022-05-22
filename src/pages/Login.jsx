cc1efbb9acb7b1d3b3767e68a511748d44ecdefb
JorgeVargas <javargas216@unicauca.edu.co>
2022-05-22 18:31:04 -0500

    Login page new styles

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,13 @@
-import React, {useContext, useRef} from "react";
+import React, { useContext, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { post } from "../api";
 import { authContext } from "../context/AuthContext";
 import { NavLink } from "react-router-dom";
 import Navbar from "../components/Navbar";
-import Logo from "../assets/svg/logo.svg"
+import Logo from "../assets/svg/logo.svg";
 
 export default function Login() {
-
-	const {auth, setAuth} = useContext(authContext);
+	const { auth, setAuth } = useContext(authContext);
 
 	const navigate = useNavigate();
 
@@ -20,58 +19,68 @@ export default function Login() {
 
 		post("/api/auth/login", {
 			email: emailRef.current.value,
-			password: passwordRef.current.value
-
+			password: passwordRef.current.value,
 		})
-		.then(({data}) => {
-			const {token,user} = data;
-			localStorage.setItem("token", token);
-			setAuth({
-				id: user.id,
-				name: user.name,
-				role: user.role,
-				email: user.email,
-				logged: true
+			.then(({ data }) => {
+				const { token, user } = data;
+				localStorage.setItem("token", token);
+				setAuth({
+					id: user.id,
+					name: user.name,
+					role: user.role,
+					email: user.email,
+					logged: true,
+				});
+				navigate("/", { replace: true });
 			})
-			navigate("/", {replace: true});
-		})
-		.catch(err => {
-			console.log(err);
-		})
-	}
+			.catch((err) => {
+				console.log(err);
+			});
+	};
 
 	return (
 		<>
-			<Navbar/>
-			<form onSubmit={login} className="container">
+			<Navbar />
+			<div className="container">
+				<form onSubmit={login}>
 
-				<img className="logo2" src={Logo} alt="job search logo" />
+					<img className="logo2" src={Logo} alt="job search logo" />
+					<h4 className="subtitle">Login</h4>
 
-				<h4 className="subtitle">Login</h4>
+					<div className="form-group">
+						<label className="lbl">Email address</label>
+						<input
+							ref={emailRef}
+							className="inp"
+							placeholder="jhondoe@domain.com"
+							type="email"
+						/>
+					</div>
 
-				<div className="form-group">
-					<label className="lbl">Email address</label>
-					<input ref={emailRef} className="inp" placeholder="jhondoe@domain.com" type="email" />
-				</div>
+					<div className="form-group">
+						<label className="lbl">Password</label>
+						<input
+							ref={passwordRef}
+							className="inp"
+							type="password"
+							placeholder="Password"
+						/>
+					</div>
 
-				<div className="form-group">
-					<label className="lbl">Password</label>
-					<input ref={passwordRef} className="inp" type="password" placeholder="Password" />
-				</div>
+					<div className="form-group">
+						<button className="btn" type="submit">
+							<span>Login</span>
+						</button>
+					</div>
 
-				<div className="form-group">
-					<button className="btn" type="submit">
-						<span>Login</span>
-					</button>
-				</div>
-
-				<div className="txt-section">
-					<p>Don't have an account yet?</p>
-					<NavLink to="/signup" className="">
-						Sing up here!
-					</NavLink>
-				</div>
-			</form>
+					<div className="txt-section">
+						<p>Don't have an account yet?</p>
+						<NavLink to="/signup" className="blue-tag">
+							Sing up here!
+						</NavLink>
+					</div>
+				</form>
+			</div>
 		</>
 	);
 }
