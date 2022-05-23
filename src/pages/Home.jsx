2c4786d6cd2cd11194d09fad096f72190ee4c886
LauraMamian <lauramamianc@unicauca.edu.co>
2022-05-22 20:16:50 -0500

    home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,56 @@ export default function Home() {
 	return (
 		<>
 			<Navbar />
-			<div className="container">
-				<h1>Home</h1>
+			<div className="section">
+				<div className="container">
+					<div className="section-title">
+						<h1>How It work</h1>
+						<p>Post a job to tell us about your project. We'll quickly match you with the right freelancers. </p>
+
+						<div className="process-menu">
+							<div className="nav-link">
+								<div className="d-flex">
+									<div className="number">
+										1
+									</div>
+
+									<div className="parrafo">
+										<h3>Register an account</h3>
+										<p>Due to its widespread use as filler text for layouts, non-readability is of great importance</p>
+									</div>
+								</div>
+							</div>
+
+							<div className="nav-link">
+								<div className="d-flex">
+									<div className="number">
+										2
+									</div>
+
+									<div className="parrafo">
+										<h3>Find your job</h3>
+										<p>There are many variations of passages of avaibookmarl-label, but the majority alteration in some form</p>
+									</div>
+								</div>
+							</div>
+
+							<div className="nav-link">
+								<div className="d-flex">
+									<div className="number">
+										3
+									</div>
+
+									<div className="parrafo">
+										<h3>Apply for job</h3>
+										<p>It is a long established fact that a reader will be distracted by the readable content of</p>
+									</div>
+								</div>
+							</div>
+						</div>
+
+					</div>
+
+				</div>
 			</div>
 		</>
 	)
