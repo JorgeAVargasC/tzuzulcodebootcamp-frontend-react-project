85bca5d2e19bca7463af3718ef1333bdda88d440
LauraMamian <lauramamianc@unicauca.edu.co>
2022-05-22 20:39:24 -0500

    home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,39 +1,45 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import img1 from "../assets/img/process-01.png"
+import img2 from "../assets/img/process-02.png"
+import img3 from "../assets/img/process-03.png"
 
 export default function Home() {
 	return (
 		<>
 			<Navbar />
 			<div className="section">
-				<div className="container">
-					<div className="section-title">
-						<h1>How It work</h1>
-						<p>Post a job to tell us about your project. We'll quickly match you with the right freelancers. </p>
+				<div className="container2">
 
-						<div className="process-menu">
-							<div className="nav-link">
-								<div className="d-flex">
-									<div className="number">
-										1
-									</div>
+					<div className="col-lg-6">
+						<div className="section-title">
+							<h1>How It work</h1>
+							<p>Post a job to tell us about your project. We'll quickly match you with the right freelancers. </p>
 
-									<div className="parrafo">
-										<h3>Register an account</h3>
-										<p>Due to its widespread use as filler text for layouts, non-readability is of great importance</p>
+							<div className="process-menu">
+								<div className="nav-link">
+									<div className="d-flex">
+										<div className="number">
+											1
+										</div>
+
+										<div className="parrafo">
+											<h3>Register an account</h3>
+											<p>Due to its widespread use as filler text for layouts, non-readability is of great importance</p>
+										</div>
 									</div>
 								</div>
-							</div>
 
-							<div className="nav-link">
-								<div className="d-flex">
-									<div className="number">
-										2
-									</div>
+								<div className="nav-link">
+									<div className="d-flex">
+										<div className="number">
+											2
+										</div>
 
-									<div className="parrafo">
-										<h3>Find your job</h3>
-										<p>There are many variations of passages of avaibookmarl-label, but the majority alteration in some form</p>
+										<div className="parrafo">
+											<h3>Find your job</h3>
+											<p>There are many variations of passages of avaibookmarl-label, but the majority alteration in some form</p>
+										</div>
 									</div>
 								</div>
 							</div>
@@ -54,6 +60,21 @@ export default function Home() {
 
 					</div>
 
+					<div className="col-lg-6">
+						<div className="tab-content">
+							<div className="images">
+								<img className="img-fluid" src={img1} alt="" width="300" />
+							</div>
+
+							<div className="images">
+								<img className="img-fluid" src={img2} alt="" width="300" />
+							</div>
+
+							<div className="images">
+								<img className="img-fluid" src={img3} alt="" width="300" />
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</>
